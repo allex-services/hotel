4d83ab0196f33019afee9adedf0a9096d307a771
Luka Petrovic <lukapetrovicsi@gmail.com>
2016-10-20 16:42:08 +0000

    AcquireUserServiceSinkTask auto-destroys itself

--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -139,27 +139,14 @@ function createAcquireUserServiceSink(execlib){
     this.sink = prophash.sink;
     this.cb = prophash.cb;
     this.propertyhash = prophash.propertyhash || {};
-    this.acquiredDestroyListener = null;
-    this.userSinkDestroyedListener = null;
     this.attempts = 0;
   }
   lib.inherit(AcquireUserServiceSinkTask,SinkTask);
   AcquireUserServiceSinkTask.prototype.__cleanUp = function () {
-    console.log('AcquireUserServiceSinkTask dying');
-    if (this.userSinkDestroyedListener) {
-      this.userSinkDestroyedListener.destroy();
-    }
-    this.userSinkDestroyedListener = null;
-    if (this.acquiredDestroyListener) {
-      this.acquiredDestroyListener.destroy();
-    }
-    this.acquiredDestroyListener = null;
+    //console.log('AcquireUserServiceSinkTask dying');
+    this.attempts = null;
     this.propertyhash = null;
     this.cb = null;
-
-    if (this.sink) {
-      this.sink.destroy();
-    }
     this.sink = null;
     SinkTask.prototype.__cleanUp.call(this);
   };
@@ -170,35 +157,32 @@ function createAcquireUserServiceSink(execlib){
   };
   AcquireUserServiceSinkTask.prototype.onSelfApartment = function (selfname) {
     this.attempts++;
-    console.log('trying to subconnect to my apartment', selfname, '#', this.attempts);
+    //console.log('trying to subconnect to my apartment', selfname, '#', this.attempts);
     this.sink.subConnect(selfname,{name:selfname,role:'user'},this.propertyhash).done(
       this.onAcquired.bind(this),
       this.onAcquireFailed.bind(this)
     );
   };
   AcquireUserServiceSinkTask.prototype.onAcquired = function(sink){
-    console.log('ok');
-    if (this.acquiredDestroyListener) {
-      this.acquiredDestroyListener.destroy();
-    }
-    this.acquiredDestroyListener = sink.destroyed.attach(this.onAcquiredSinkDown.bind(this));
-    this.cb(sink);
-    if (!sink) {
-      console.log('no sink?');
-      this.destroy();
-    } else {
-      if (this.userSinkDestroyedListener) {
-        this.userSinkDestroyedListener.destroy();
+    //console.log('ok');
+    if (this.cb) {
+      this.cb(sink);
+      if (!sink) {
+        console.log('no sink?');
+      } else {
+        if (sink.destroyed) {
+          sink.destroyed.attachForSingleShot(this.sink.destroy.bind(this.sink));
+        }
       }
-      this.userSinkDestroyedListener = sink.destroyed.attach(this.destroy.bind(this));
     }
+    this.destroy();
   };
   AcquireUserServiceSinkTask.prototype.onAcquireFailed = function (reason) {
     console.log('onAcquireFailed',arguments);
+    if (this.cb) {
+      this.cb(null);
+    }
     this.destroy();
-  };
-  AcquireUserServiceSinkTask.prototype.onAcquiredSinkDown = function () {
-    this.cb(null);
   };
   AcquireUserServiceSinkTask.prototype.compulsoryConstructionProperties = ['sink', 'cb'];
 
